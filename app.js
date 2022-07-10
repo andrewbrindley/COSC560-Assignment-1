3b684ed977f9d048af10afba84db158d88512669
andrewbrindley <a.brindley@hotmail.com>
2022-07-10 18:35:03 +1000

    added swaps between black and white

--- app.js
+++ app.js
@@ -8,39 +8,58 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     }
     return to.concat(ar || Array.prototype.slice.call(from));
 };
-var _a;
 var Controller = /** @class */ (function () {
     function Controller() {
+        this.turn = 0;
+        this.game = new Game(this.turn, 15, 15);
     }
     return Controller;
 }());
-var Tile = /** @class */ (function () {
-    function Tile(row, column) {
-        this.row = row;
-        this.column = column;
-        this.element = document.createElement('div');
-        this.element.classList.add('tile');
-        this.element.addEventListener("click", function (e) {
-            console.log(row, column);
-        }, false);
+var Game = /** @class */ (function () {
+    function Game(turn, rows, columns) {
+        var _this = this;
+        this.tileClicked = function (tile) {
+            if (tile.value < 0) {
+                tile.value = _this.turn;
+                tile.element.classList.remove('empty');
+                tile.element.classList.add(!_this.turn ? 'black' : 'white');
+                _this.turn = (_this.turn + 1) % 2;
+            }
+        };
+        this.turn = turn;
+        this.grid = new Grid(rows, columns, this.tileClicked);
     }
-    return Tile;
+    return Game;
 }());
 var Grid = /** @class */ (function () {
-    function Grid(rows, columns) {
+    function Grid(rows, columns, tileClicked) {
         var _a;
         var _this = this;
+        var _b;
         this.rows = rows;
         this.columns = columns;
-        this.tiles = __spreadArray([], Array(this.rows), true).map(function (_, i) { return __spreadArray([], Array(_this.columns), true).map(function (_, j) { return new Tile(i, j); }); });
-        this.flatTiles = this.tiles.reduce(function (a, v) { return a.concat(__spreadArray([], v, true)); }, []);
+        this.tiles = __spreadArray([], Array(this.rows), true).map(function (_, i) { return __spreadArray([], Array(_this.columns), true).map(function (_, j) { return new Tile(i, j, tileClicked); }); });
         this.element = document.createElement('div');
         this.element.classList.add('grid');
         this.element.style.gridTemplateRows = "repeat(".concat(this.rows, ", 1fr)");
         this.element.style.gridTemplateColumns = "repeat(".concat(this.columns, ", 1fr)");
-        (_a = this.element).append.apply(_a, this.flatTiles.map(function (t) { return t.element; }));
+        (_a = this.element).append.apply(_a, this.tiles.reduce(function (a, v) { return a.concat(__spreadArray([], v, true)); }, []).map(function (t) { return t.element; }));
+        (_b = document.getElementById('game')) === null || _b === void 0 ? void 0 : _b.appendChild(this.element);
     }
     return Grid;
 }());
-var myGrid = new Grid(15, 15);
-(_a = document.getElementById('game')) === null || _a === void 0 ? void 0 : _a.appendChild(myGrid.element);
+var Tile = /** @class */ (function () {
+    function Tile(row, column, tileClicked) {
+        var _this = this;
+        this.row = row;
+        this.column = column;
+        this.value = -1;
+        this.element = document.createElement('div');
+        this.element.classList.add('empty');
+        this.element.addEventListener('click', function (_) {
+            tileClicked(_this);
+        });
+    }
+    return Tile;
+}());
+var root = new Game(0, 15, 15);
--- app.ts
+++ app.ts
@@ -1,42 +1,71 @@
 class Controller{
+    turn: number
+    game: Game
 
-}
-
-class Tile{
-    row: number
-    column: number
-    element: HTMLDivElement
-
-    constructor(row: number, column: number){
-        this.row = row;
-        this.column = column;
-        this.element = document.createElement('div');
-        this.element.classList.add('tile');
-        this.element.addEventListener("click", function(e) {
-            console.log(row, column)
-        }, false);
+    constructor(){
+        this.turn = 0;
+        this.game = new Game(this.turn, 15, 15);
     }
 }
+
+class Game{
+    turn: number
+    grid: Grid
+
+    constructor(turn: number, rows: number, columns: number){
+        this.turn = turn;
+        this.grid = new Grid(rows, columns, this.tileClicked);
+    }
+
+    tileClicked = (tile: Tile): void => {
+        if (tile.value < 0){
+            tile.value = this.turn;
+            tile.element.classList.remove('empty');
+            tile.element.classList.add(!this.turn ? 'black' : 'white');
+            this.turn = (this.turn + 1) % 2;
+        }
+    }
+
+}
+
 
 class Grid{
     rows: number
     columns: number
     element: HTMLDivElement
     tiles: Tile[][]
-    flatTiles: Tile[]
 
-    constructor(rows: number, columns: number){
+    constructor(rows: number, columns: number, tileClicked: (tile: Tile) => void){
         this.rows = rows;
         this.columns = columns;
-        this.tiles = [...Array(this.rows)].map((_, i) => [...Array(this.columns)].map((_, j) => new Tile(i, j)));
-        this.flatTiles = this.tiles.reduce((a, v) => a.concat([...v]), []);
+        this.tiles = [...Array(this.rows)].map((_, i) => [...Array(this.columns)].map((_, j) => new Tile(i, j, tileClicked)));
         this.element = document.createElement('div');
         this.element.classList.add('grid');
         this.element.style.gridTemplateRows = `repeat(${this.rows}, 1fr)`;
         this.element.style.gridTemplateColumns = `repeat(${this.columns}, 1fr)`;
-        this.element.append(...this.flatTiles.map(t => t.element));
+        this.element.append(...this.tiles.reduce((a, v) => a.concat([...v]), []).map(t => t.element));
+        document.getElementById('game')?.appendChild(this.element);
+    }
+    
+}
+
+class Tile{
+    row: number
+    column: number
+    value: number
+    element: HTMLDivElement
+
+    constructor(row: number, column: number, tileClicked: (tile: Tile) => void){
+        this.row = row;
+        this.column = column;
+        this.value = -1;
+        this.element = document.createElement('div');
+        this.element.classList.add('empty');
+        this.element.addEventListener('click', _ => {
+            tileClicked(this);
+        });
     }
 }
 
-const myGrid = new Grid(15, 15);
-document.getElementById('game')?.appendChild(myGrid.element);
+
+const root = new Game(0, 15, 15);
